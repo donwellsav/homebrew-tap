4c497fa0424f8ca5ec2e9163bf386d201a12dcf2
Don Wells <donwellsav@gmail.com>
2026-08-24 12:13:49 -0400

    donwells-cue 2.6.2

--- Casks/donwells-cue.rb
+++ Casks/donwells-cue.rb
@@ -1,9 +1,9 @@
 cask "donwells-cue" do
-  version "2.6.1"
+  version "2.6.2"
   arch arm: "arm64", intel: "x64"
 
-  sha256 arm:   "4c6de96135ff5c0a858fe998c05a6a1c2902e42e91e966ba4389907fba59dd5b",
-         intel: "608d6012e61eb55847111f3e79963803fa9dd68570afee5ce2356fdd2e1e6eff"
+  sha256 arm:   "c7b91214c18d17fb5448cce5ab388b4b28a21bd6a1d99d55354db29bf4970305",
+         intel: "5ff7fa38b3b7efe52b7f3bc78c8b82759564294f1cb1f6e0fb21a4e8cdeb75f1"
 
   url "https://github.com/donwellsav/dwcue/releases/download/v#{version}/DonWells-Cue-#{version}-#{arch}.dmg"
 
